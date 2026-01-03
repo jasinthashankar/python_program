be9c7eba9c872b1f3364d3e9fb32d3f6e77076af
unknown <jasinthashankar.com>
2026-01-03 14:18:31 +0530

    added

---
+++ oddoreven.py
@@ -0,0 +1,9 @@
+# Get input from user
+number = int(input("Enter a number: "))
+
+# Check if the number is odd or even
+if number % 2 == 0:
+    print(f"{number} is even")
+else:
+    print(f"{number} is odd")
+    
